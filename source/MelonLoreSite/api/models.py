d3a854dbe8e92f1de7c6b70c2067ec28de052af5
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-24 21:06:18 -0700

    finished models design

--- source/MelonLoreSite/api/models.py
+++ source/MelonLoreSite/api/models.py
@@ -9,8 +9,50 @@ from django.db import models
 # URL to Devlog video playlist (optional)
 # A link to a Design Document
 # A list of tags for future searches
-class DetailedBase(models.Model):
+class BaseArticle(models.Model):
     name = models.CharField(max_length=255, default="")
+    related_video = models.CharField(max_length=255)
+    related_image = models.CharField(max_length=255)
+    
+
+# Base for article entries 
+# Title
+# Related Video URL
+# Related Image link or URL
+# DevLog Body
+# Date of Devlog
+# Date Created
+# Date Modified
+class BaseEntry(models.Model):
+    title = models.CharField(max_length=255)
+    related_video = models.CharField(max_length=255)
+    related_image = models.CharField(max_length=255)
+    main_body = models.TextField()
+    date = models.DateTimeField()
+    modified = models.DateTimeField()
+    created = models.DateTimeField()
+
+
+# Tags
+# A collection of desctiptors
+# tag name
+# tag description
+
+# Technologies
+# A collection of relevant technologies
+# tech name
+# tech description
+
+# TechnologiesToArticles
+# Many to Many
+# tech id
+# article id
+
+# TagsToArticles
+# Many to Many
+# tag id
+# article id
+
 
 # Projects:
 # We need a name
@@ -24,17 +66,24 @@ class DetailedBase(models.Model):
 # A link to a Design Document
 # A list of tags for future searches
 
-# DevLogContainer:
+# ProjectToDevLog
+
+# DevLog:
 # We need a name
 # A list of technologies used
-# A URL to video of demo
-# A link to related dev logs
+# A URL to related Video
 # A brief description
-# A Project Article about any hardships encountered
+# Linked Project
 # A Github URL
 # URL to Devlog video playlist (optional)
 # A link to a Design Document
 # A list of tags for future searches
+
+
+# Devlog to Entries
+# DevLog ID
+# DevLogEntry ID
+# one to many
 
 # DevLogEntry
 # Title
@@ -44,3 +93,26 @@ class DetailedBase(models.Model):
 # Date of Devlog
 # Date Created
 # Date Modified
+
+# TutorialSeries
+# We need a name
+# A list of technologies used
+# A URL to video of tutorial
+# A link to related dev logs
+# A brief description
+# Tutorial Body
+# A Github URL
+# URL to Tutorial video playlist (optional)
+# A link to Additional resources
+# A list of tags for future searches
+
+# TutorialEntry
+# Title
+# Related Video URL
+# Related Image link or URL
+# Tutorial Body
+# Date of Tutorial
+# Date Created
+# Date Modified
+
+# TutorialToEntries
