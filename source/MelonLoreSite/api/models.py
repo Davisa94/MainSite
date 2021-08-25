a5a955794f7e5047c708d60d13dd711703c87220
Davisa94 <Austin.i.davis94@gmail.com>
2021-08-24 18:53:47 -0700

    Outlined requirments for the models

--- source/MelonLoreSite/api/models.py
+++ source/MelonLoreSite/api/models.py
@@ -4,14 +4,43 @@ from django.db import models
 
 # We need a name
 # A list of technologies used
-# A link to video of demo
+# A URL to video of demo
+# A Github URL
+# URL to Devlog video playlist (optional)
+# A link to a Design Document
+# A list of tags for future searches
+class DetailedBase(models.Model):
+    name = models.CharField(max_length=255, default="")
+
+# Projects:
+# We need a name
+# A list of technologies used
+# A URL to video of demo
 # A link to related dev logs
 # A brief description
 # A Project Article about any hardships encountered
-# A Github link
-# Link to Devlog video playlist (optional)
+# A Github URL
+# URL to Devlog video playlist (optional)
 # A link to a Design Document
-class Project(models.Model):
-    name = models.CharField(max_length=255, default="")
+# A list of tags for future searches
 
+# DevLogContainer:
+# We need a name
+# A list of technologies used
+# A URL to video of demo
+# A link to related dev logs
+# A brief description
+# A Project Article about any hardships encountered
+# A Github URL
+# URL to Devlog video playlist (optional)
+# A link to a Design Document
+# A list of tags for future searches
 
+# DevLogEntry
+# Title
+# Related Video URL
+# Related Image link or URL
+# DevLog Body
+# Date of Devlog
+# Date Created
+# Date Modified
